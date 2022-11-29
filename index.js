6a39b1ca9d5fe72e943d1f2029fa5a43a9a207db
Ashergreen82 <greena@rogers.com>
2022-11-29 07:14:48 -0500

    created event listeners for numbers
    Still need to create event listeners for the rest of the buttons.

--- index.js
+++ index.js
@@ -1,40 +1,57 @@
 const calculatorContainer = document.getElementById("calculator_container");
-const CalculatorDisplay = document.getElementById("calculator_display");
+const calculatorDisplay = document.getElementById("calculator_display");
+// const numberButtonElement = document.getElementById("number_button_7");
+const numberButtonElement = document.getElementsByClassName("number_button");
+const operatorButtonElement = document.getElementsByClassName("operator_button");
+const functionButtonElement = document.getElementsByClassName("function_button");
+const answerButtonElement = document.getElementsByClassName("answer_button");
+const memoryButtonElement = document.getElementsByClassName("memory_button");
+let num1 = 0
+let num2 = 0
+let operator = null
 let numberTobeDisplayed = "0";
 let totalItemsDisplayed = "0";
-
+// numberButtonElement.addEventListener("click", buttonSelected);
+for (i = 0; i < 10; i++) {
+    numberButtonEventListenerCreator = document.getElementById(`number_button_${i}`);
+    numberButtonEventListenerCreator.addEventListener("click", buttonSelected);
+}
+// operatorButtonElement.addEventListener("click", buttonSelected);
+// functionButtonElement.addEventListener("click", buttonSelected);
+// answerButtonElement.addEventListener("click", buttonSelected);
+// memoryButtonElement.addEventListener("click", buttonSelected);
 // number button creation (Alt + 251 = square root and Alt + 0247 = division symbol)
-buttonCreator("MC");
-buttonCreator("MR");
-buttonCreator("M-");
-buttonCreator("M+");
-buttonCreator("√");
-for (i = 7; i < 10; i++) {
-    buttonCreator(i);
-}
-buttonCreator("÷");
-buttonCreator("+/-");
-for (i = 4; i < 7; i++) {
-    buttonCreator(i);
-}
-buttonCreator("X");
-buttonCreator("C");
-for (i = 1; i < 4; i++) {
-    buttonCreator(i);
-}
-buttonCreator("-");
-buttonCreator("AC");
-buttonCreator(0);
-buttonCreator(".");
-buttonCreator("=");
-buttonCreator("+");
+// buttonCreator("MC");
+// buttonCreator("MR");
+// buttonCreator("M-");
+// buttonCreator("M+");
+// buttonCreator("√");
+// for (i = 7; i < 10; i++) {
+//     buttonCreator(i);
+// }
+// buttonCreator("÷");
+// buttonCreator("+/-");
+// for (i = 4; i < 7; i++) {
+//     buttonCreator(i);
+// }
+// buttonCreator("X");
+// buttonCreator("C");
+// for (i = 1; i < 4; i++) {
+//     buttonCreator(i);
+// }
+// buttonCreator("-");
+// buttonCreator("AC");
+// buttonCreator(0);
+// buttonCreator(".");
+// buttonCreator("=");
+// buttonCreator("+");
 
 totalItemsDisplayed = calculatorDisplayProcess(numberTobeDisplayed, totalItemsDisplayed);
 
 //functions
 function calculatorDisplayProcess(numberTobeDisplayed, totalItemsDisplayed) {
     totalItemsDisplayed = totalItemsDisplayed + numberTobeDisplayed;
-    CalculatorDisplay.innerText = totalItemsDisplayed;
+    calculatorDisplay.innerText = totalItemsDisplayed;
     return totalItemsDisplayed;
 }
 // number display
@@ -42,9 +59,9 @@ function buttonSelected(e) {
     e.preventDefault();
     buttonPressed = e.srcElement.firstChild.textContent;
     console.log(buttonPressed);
-    return buttonPressed;
+    num1 = buttonPressed;
     // console.log(e);
-    // console.log(createNumberButton);
+    // console.log(numberButtonElement);
     // console.log("Button pressed");
     // console.log(e.target);
     // console.log(e.srcElement.firstChild.innertext);
@@ -52,12 +69,17 @@ function buttonSelected(e) {
     // console.log(e.srcElement.firstChild.textContent);
 
 }
+function eventListenerCreator(buttonForEventListener) {
+    button = document.getElementById(`number_button_${buttonLabel}`);
+
+    button.addEventListener("click", buttonSelected)
+}
 
 function buttonCreator(buttonLabel) {
-    const createNumberButton = document.createElement("button");
-    createNumberButton.innerText = buttonLabel;
-    createNumberButton.setAttribute("class", "number_button");
-    createNumberButton.setAttribute("id", `number_button_${buttonLabel}`);
-    calculatorContainer.appendChild(createNumberButton);
-    numberTobeDisplayed = createNumberButton.addEventListener("click", buttonSelected);
+    const createButtonElement = document.createElement("button");
+    createButtonElement.innerText = buttonLabel;
+    createButtonElement.setAttribute("class", "number_button");
+    createButtonElement.setAttribute("id", `number_button_${buttonLabel}`);
+    calculatorContainer.appendChild(numberButtonElement);
+    numberTobeDisplayed = numberButtonElement.addEventListener("click", buttonSelected);
 }
