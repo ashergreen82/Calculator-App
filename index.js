e368063678986ea5624cefed681c03d30797013c
Ashergreen82 <greena@rogers.com>
2022-12-02 07:21:18 -0500

    working on the addition operator

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// IMPORTANT: Alt + 251 = square root and Alt + 0247 = division symbol
 const calculatorContainer = document.getElementById("calculator_container");
 const calculatorDisplay = document.getElementById("calculator_display");
 // const numberButtonElement = document.getElementById("number_button_7");
@@ -6,55 +7,118 @@ const operatorButtonElement = document.getElementsByClassName("operator_button")
 const functionButtonElement = document.getElementsByClassName("function_button");
 const answerButtonElement = document.getElementsByClassName("answer_button");
 const memoryButtonElement = document.getElementsByClassName("memory_button");
-let num1 = 0
-let num2 = 0
-let operator = null
-let numberTobeDisplayed = "0";
+const memoryButtonPressed = document.querySelectorAll(".memory_button");
+const numberButtonPressed = document.querySelectorAll(".number_button");
+const operatorButtonPressed = document.querySelectorAll(".operator_button");
+const functionButtonPressed = document.querySelectorAll(".function_button");
+const answerButtonPressed = document.querySelectorAll(".answer_button");
+const allButtons = document.querySelectorAll("button");
+let operator = null;
+let numberTobeDisplayed = "";
 let totalItemsDisplayed = "0";
-// numberButtonElement.addEventListener("click", buttonSelected);
-// for (i = 0; i < 10; i++) {
-//     numberButtonEventListenerCreator = document.getElementById(`number_button_${i}`);
-//     numberButtonEventListenerCreator.addEventListener("click", buttonSelected);
-// }
+let answer = 0;
+let flag = 0;
+let flagOperater = 0;
+let number1 = 0;
+let number2 = 0;
 
-const calculatorButtonsList = document.querySelectorAll("#calculator_container");
-const calculatorButtonsList1 = document.querySelectorAll(".memory_button");
-console.log(calculatorButtonsList);
-console.log(calculatorButtonsList1);
-for (i = 0; i < calculatorButtonsList1.length; i++) {
-    x = document.getElementById(calculatorButtonsList1[i]);
-    x.addEventListener("click", buttonSelected);
+
+console.log("All button elements: ", allButtons);
+
+memoryButtonPressed.forEach((element) => {
+    element.addEventListener("click", memoryButtonClicked);
+})
+numberButtonPressed.forEach((element) => {
+    element.addEventListener("click", numberButtonClicked);
+})
+operatorButtonPressed.forEach((element) => {
+    element.addEventListener("click", operatorButtonClicked);
+})
+functionButtonPressed.forEach((element) => {
+    element.addEventListener("click", functionButtonClicked);
+})
+answerButtonPressed.forEach((element) => {
+    element.addEventListener("click", answerButtonClicked);
+})
+
+function memoryButtonClicked(e) {
+    console.log(e.target.innerText);
 }
+
+function numberButtonClicked(e) {
+    let temporaryNumberVariable = e.target.innerText;
+    console.log(e.target.innerText);
+    if (flag === 1) {
+        numberTobeDisplayed = "";
+        calculatorDisplay.innerText = numberTobeDisplayed;
+        flag = 0;
+    }
+    if (numberTobeDisplayed.length >= 1 && operator === null) {
+        // numberTobeDisplayed = numberTobeDisplayed + temporaryNumberVariable;
+        numberTobeDisplayed += temporaryNumberVariable;
+    } else if (numberTobeDisplayed.length >= 1 && operator == "+") {
+        answer += numberTobeDisplayed;
+        numberTobeDisplayed += temporaryNumberVariable;
+        console.log(`the answer so far is ${answer}`);
+    }
+    else {
+        numberTobeDisplayed = temporaryNumberVariable;
+        calculatorDisplay.innerText = numberTobeDisplayed;
+    }
+    console.log(`this is the number to be displayed ${numberTobeDisplayed}`);
+    console.log(`This is the length of the number to be displayed ${numberTobeDisplayed.length}`);
+    calculatorDisplay.innerText = numberTobeDisplayed;
+}
+
+function operatorButtonClicked(e) {
+    let temporaryOperatorVariable = e.target.innerText;
+    console.log(e.target.innerText);
+    if (temporaryOperatorVariable === "+") {
+        operator = "+";
+        numberTobeDisplayed = "";
+        flagOperater = 1;
+    }
+}
+
+function functionButtonClicked(e) {
+    let temporaryFunctionVariable = e.target.innerText;
+    console.log(e.target.innerText);
+    if (temporaryFunctionVariable === "C") {
+        numberTobeDisplayed = "0";
+        calculatorDisplay.innerText = numberTobeDisplayed;
+        flag = 1
+    }
+}
+function answerButtonClicked(e) {
+    let temporaryFunctionVariable = e.target.innerText;
+    console.log(e.target.innerText);
+    console.log(answer);
+    if (temporaryFunctionVariable === "=") {
+        calculatorDisplay.innerText = answer;
+        flag = 0;
+    }
+}
+
+function memoryButtonClicked(e) {
+    let temporaryFunctionVariable = e.target.innerText;
+    console.log(e.target.innerText);
+    console.log(answer);
+}
+
+// const calculatorButtonsList = document.querySelectorAll("#calculator_container");
+// const calculatorButtonsList1 = document.querySelectorAll(".memory_button");
+// console.log(calculatorButtonsList);
+// console.log(calculatorButtonsList1);
+// for (i = 0; i < calculatorButtonsList1.length; i++) {
+//     x = document.getElementById(calculatorButtonsList1[i]);
+//     x.addEventListener("click", buttonSelected);
+// }
 
 // operatorButtonElement.addEventListener("click", buttonSelected);
 // functionButtonElement.addEventListener("click", buttonSelected);
 // answerButtonElement.addEventListener("click", buttonSelected);
 // memoryButtonElement.addEventListener("click", buttonSelected);
-// number button creation (Alt + 251 = square root and Alt + 0247 = division symbol)
-// buttonCreator("MC");
-// buttonCreator("MR");
-// buttonCreator("M-");
-// buttonCreator("M+");
-// buttonCreator("√");
-// for (i = 7; i < 10; i++) {
-//     buttonCreator(i);
-// }
-// buttonCreator("÷");
-// buttonCreator("+/-");
-// for (i = 4; i < 7; i++) {
-//     buttonCreator(i);
-// }
-// buttonCreator("X");
-// buttonCreator("C");
-// for (i = 1; i < 4; i++) {
-//     buttonCreator(i);
-// }
-// buttonCreator("-");
-// buttonCreator("AC");
-// buttonCreator(0);
-// buttonCreator(".");
-// buttonCreator("=");
-// buttonCreator("+");
+
 
 totalItemsDisplayed = calculatorDisplayProcess(numberTobeDisplayed, totalItemsDisplayed);
 
@@ -65,11 +129,11 @@ function calculatorDisplayProcess(numberTobeDisplayed, totalItemsDisplayed) {
     return totalItemsDisplayed;
 }
 // number display
-function buttonSelected(e) {
-    e.preventDefault();
-    buttonPressed = e.srcElement.firstChild.textContent;
-    console.log(buttonPressed);
-    num1 = buttonPressed;
+// function buttonSelected(e) {
+//     e.preventDefault();
+//     buttonPressed = e.srcElement.firstChild.textContent;
+//     console.log(buttonPressed);
+//     num1 = buttonPressed;
     // console.log(e);
     // console.log(numberButtonElement);
     // console.log("Button pressed");
@@ -78,18 +142,10 @@ function buttonSelected(e) {
     // console.log(e.srcElement.firstChild.nodeValue);
     // console.log(e.srcElement.firstChild.textContent);
 
-}
-function eventListenerCreator(buttonForEventListener) {
-    button = document.getElementById(`number_button_${buttonLabel}`);
+// }
+// function eventListenerCreator(buttonForEventListener) {
+//     button = document.getElementById(`number_button_${buttonLabel}`);
 
-    button.addEventListener("click", buttonSelected)
-}
+//     button.addEventListener("click", buttonSelected)
+// }
 
-function buttonCreator(buttonLabel) {
-    const createButtonElement = document.createElement("button");
-    createButtonElement.innerText = buttonLabel;
-    createButtonElement.setAttribute("class", "number_button");
-    createButtonElement.setAttribute("id", `number_button_${buttonLabel}`);
-    calculatorContainer.appendChild(numberButtonElement);
-    numberTobeDisplayed = numberButtonElement.addEventListener("click", buttonSelected);
-}
