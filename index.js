758eada2f731ef5a7d1c92b8c1637829b41be69d
Ashergreen82 <greena@rogers.com>
2022-12-04 20:44:09 -0500

    Got the multiple addition to work.

--- index.js
+++ index.js
@@ -58,11 +58,16 @@ function numberButtonClicked(e) {
         if (number2 === null) {
             //          set second mumber to the button that was pressed
             number2 = temporaryNumberVariable;
+        } else if (number2 != null && flagOperater > 1 && operator === "+") {
+            calculator_display.innerText = temporaryNumberVariable;
+            // additionOperation();
+            // calculatorDisplay.innerText = answer
         } else { // else
             //          second number is not empty:  Append to the second number
             number2 += temporaryNumberVariable;
+            calculatorDisplay.innerText = number2
         }
-        calculatorDisplay.innerText = number2
+
         console.log(`The answer so far is ${answer}`);
     }
     if (operator == null) {
@@ -71,33 +76,6 @@ function numberButtonClicked(e) {
         calculatorDisplay.innerText = numberTobeDisplayed;
         number1 = numberTobeDisplayed; // set first number to the button that was pressed
     }
-    // else
-    // append button that was pressed to first number
-
-    //  render the first number in the display
-
-    // if (flagOperater === 1) {
-
-    // }
-    // if (flag === 1) {
-    //     numberTobeDisplayed = "";
-    //     calculatorDisplay.innerText = numberTobeDisplayed;
-    //     flag = 0;
-    // }
-    // if (temporaryNumberVariable.length >= 1 && operator === null) {
-    //     // numberTobeDisplayed = numberTobeDisplayed + temporaryNumberVariable;
-    //     numberTobeDisplayed += temporaryNumberVariable;
-    // } else if (temporaryNumberVariable.length >= 1 && operator == "+") {
-
-    // }
-    // else {
-    //     numberTobeDisplayed = temporaryNumberVariable;
-    //     calculatorDisplay.innerText = numberTobeDisplayed;
-    // }
-    // console.log(`This is the number to be displayed: ${numberTobeDisplayed}`);
-    // console.log(`This is the length of the number to be displayed: ${numberTobeDisplayed.length}`);
-    // calculatorDisplay.innerText = numberTobeDisplayed;
-    // number1 = Number(numberTobeDisplayed);
 }
 
 // Operator Function
@@ -105,26 +83,13 @@ function operatorButtonClicked(e) {
     let temporaryOperatorVariable = e.target.innerText;
     console.log(e.target.innerText);
 
-    // Addition
+    // + operator
     if (temporaryOperatorVariable === "+") {
-        if (flagOperater == 1) {
-            answer = Number(number1) + Number(number2);
-        } else if (flagOperater > 1) {
-            answer += Number(number2);
-        }
-        flagOperater += 1;
-        operator = "+";
-        if (flagOperater > 1) {
-            numberTobeDisplayed = answer
-            calculatorDisplay.innerText = numberTobeDisplayed;
-        } else {
-            numberTobeDisplayed = "";
-        }
+        additionOperation();
     }
-
-    // Subtraction
-    // Multiplication
-    // Division
+    // - operator
+    // x operator
+    // ÷ operator
 }
 
 // Clear Function
@@ -140,25 +105,13 @@ function functionButtonClicked(e) {
 // Equals Function
 function equalButtonClicked(e) {
     let temporaryFunctionVariable = e.target.innerText;
-    console.log(e.target.innerText);
+    console.log(temporaryFunctionVariable);
     console.log(answer);
     // if (operator is equal to plus sign)
     if (operator == "+") {
-        if (flagOperater > 1) {
-            calculatorDisplay.innerText = answer;
-        } else {
-            answer = Number(number1) + Number(number2);
-            calculatorDisplay.innerText = answer;
-            flagEqual = 1;
-        }
-        // partialReset();
-        // operator = null;
-        // number1 = null;
-        // number2 = null;
-        // flagOperater = 0;
+        additionOperation();
     }
-    // then add number and number2 together
-    // display result on the display
+    flagEqual = 1
 }
 
 // Clear display
@@ -188,23 +141,23 @@ function calculatorDisplayProcess(numberTobeDisplayed, totalItemsDisplayed) {
     calculatorDisplay.innerText = totalItemsDisplayed;
     return totalItemsDisplayed;
 }
-// number display
-// function buttonSelected(e) {
-//     e.preventDefault();
-//     buttonPressed = e.srcElement.firstChild.textContent;
-//     console.log(buttonPressed);
-//     num1 = buttonPressed;
-    // console.log(e);
-    // console.log(numberButtonElement);
-    // console.log("Button pressed");
-    // console.log(e.target);
-    // console.log(e.srcElement.firstChild.innertext);
-    // console.log(e.srcElement.firstChild.nodeValue);
-    // console.log(e.srcElement.firstChild.textContent);
 
-// }
-// function eventListenerCreator(buttonForEventListener) {
-//     button = document.getElementById(`number_button_${buttonLabel}`);
+function additionOperation() {
+    if (flagOperater == 1) {
+        answer = Number(number1) + Number(number2);
+        calculator_display.innerText = answer;
+    } else if (flagOperater > 1) {
+        answer += Number(number2);
+        numberTobeDisplayed = answer;
+        calculatorDisplay.innerText = numberTobeDisplayed;
+    } else {
+        numberTobeDisplayed = "";
+    }
+    // else {
+    //     answer = Number(number1) + Number(number2);
+    //     calculatorDisplay.innerText = answer;
+    //     flagEqual = 1;
+    operator = "+";
+    flagOperater += 1;
+}
 
-//     button.addEventListener("click", buttonSelected)
-// }
